1ae1a0bae27c18c819ee6b1797f8d2ebb04ae9b3
Alexander Procenko <alexanderzaytcev89@yandex.ru>
2022-05-09 20:49:34 +1000

    изменения

--- src/Main.java
+++ src/Main.java
@@ -6,30 +6,41 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Привет! Я счётчик шагов");
 
-        System.out.println("Цель шагов в месяц?");
-       int stepsMonth = scanner.nextInt();
+        System.out.println("Ваша цель шагов в месяц?");
+        int stepsMonth = scanner.nextInt();
 
-        while(true) {
+        while (true) {
             printMenu();
-            int command = scanner.nextInt()
+            int command = scanner.nextInt();
 
             if (command == 1) {
-                System.out.println("Введите количество шагов за день ");
-                int steps = scanner.nextInt();
-            if (command == 2) {
-                System.out.println("Напечатать статистику за определенный месяц");
+                System.out.println("За какой день вы хотите ввести колличество шагов: 1-ПН, 2-ВТ, 3-СР, 4-ЧТ, 5-ПТ, 6-СБ, 7-ВС?");
+                int day = scanner.nextInt();
+                System.out.println("Введите колличество шагов:");
+                int numberOfSteps = scanner.nextInt();
+                stepsMonth = stepsMonth - numberOfSteps;
+                System.out.println("Значение сохранено! Ваше коллисество шагов за месяц: " + stepsMonth);
+            } else if (command == 2) {
+                System.out.println("Выберите, за какой месяц вы хотите получить статистику: 1-Январь, 2-Февраль, 3-Март, 4-Апрель, 5-Май, 6-Июнь, 7-Июль, 8-Август, 9-Сентябрь, 10-Октябрь, 11-Ноябрь, 12-Декабрь");
+                int month = scanner.nextInt();
+                System.out.println(stepsMonth)
             }
-            if (command == 3) {
-                System.out.println("Изменить цель по колличеству шагов в день");
-            }
-            if (command == 4) {
-                System.out.println("Выйти из приложения");
-            }
-            printMenu(); // печатем меню ещё раз перед завершением предыдущего действия
-            userInput = scanner.nextInt(); // повторное считывание данных от пользователя
+        } else if (command == 3) {
+            stepsmonth = stepTracker.newstepsMonth();
+            System.out.println("Цель по количеству шагов изменено.");
+            System.out.println("Ваша новая цель - " + stepsMonth + " в день!");
         }
+    } else if(command ==4)
+
+    {
+        System.out.println("Выйти из приложения");
+        break;
+    } else{
         System.out.println("Программа завершена");
-    }
+              }
+
+                }
+                }
 
     private static void printMenu() {
         System.out.println("Что вы хотите сделать? ");
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,2 +1,15 @@
 public class StepTracker {
+
+    MonthData[] monthToData;
+
+    public StepTracker() {
+        monthToData = new MonthData[12];
+        for (int i = 0; i < monthToData; i++) {
+            monthToData[i] = new MonthData();
+        }
+    }
+
+    class MonthData {
+        // Заполните класс самостоятельно
+    }
 }
